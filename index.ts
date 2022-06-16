b7f20cb6e378eaf3af845ca859f320f114345c6d
chessdude1 <mitia.platonow2011@yandex.ru>
2022-06-16 13:38:35 +0300

    feat: start server

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 require('dotenv').config();
-
+import http from 'http'
 const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Сервер выключен')
+})
+
+server.listen(PORT, () => console.log(`Server is started on PORT: ${PORT}`))
